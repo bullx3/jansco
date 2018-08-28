1da81d3a7539496dad77412e78a16b0962ffc31d
tom_surp4 <ryuko2010@gmail.cm>
2018-08-28 13:26:35 +0900

    マイナス表示をテンプレート化。集計表示をルームでグループ化

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -14,8 +14,7 @@ class PersonalController < ApplicationController
 	sections = Section.where(status: Section::Status::FINISHED)
 
 	sections = sections.joins(games: [:scores]).select("sections.*, games.*, scores.*")
-#	sections = sections.where("sections.games.scores.player_id IN(?)", player_ids)
-#	sections = sections.where("games.scores.player_id = ?", player_ids)
+	sections = sections.includes(:group)
 	sections = sections.where("player_id IN(?)", player_ids)
 
 	profits = sections.select("SUM(rate * score) as sum_profit")
@@ -23,18 +22,16 @@ class PersonalController < ApplicationController
 	profits = profits.select("COUNT(scorekind = #{Game::Scorekind::GAME} OR NULL) as count_game")
 	profits = profits.where.not(rate: nil)
 
-	@profit_total = profits
-	logger.debug("@profit_total.length:#{@profit_total.length}")
-	logger.debug("count_game:#{@profit_total[0].count_game}")
-	logger.debug("sum_score:#{@profit_total[0].sum_score}")
-	logger.debug("sum_profit:#{@profit_total[0].sum_profit}")
+	@profit_totals = profits.group(:group_id)
+
+	logger.debug("@profit_totals.length:#{@profit_totals.length}")
 
 	# 月ごとの集計 実際は 12時間後(例 2018/8分は 8/1 12:00 ～ 9/1 11:59までに終了した対局)
 	# JST での時間で集計する為 +9Hで判定する（MySQLはUTCで保存）
-	@profit_months = profits.select("DATE_FORMAT((finished_at - INTERVAL 12 HOUR + INTERVAL 9 HOUR), '%Y/%m') as monthly").group("monthly")
+	@profit_months = profits.select("DATE_FORMAT((finished_at - INTERVAL 12 HOUR + INTERVAL 9 HOUR), '%Y/%m') as monthly").group("monthly", :group_id).order("monthly asc")
 	logger.debug("@profit_months.length:#{@profit_months.length}")
 
-	@profit_dailys = profits.select("DATE_FORMAT((finished_at - INTERVAL 12 HOUR + INTERVAL 9 HOUR), '%Y/%m/%d') as daily").group("daily")
+	@profit_dailys = profits.select("DATE_FORMAT((finished_at - INTERVAL 12 HOUR + INTERVAL 9 HOUR), '%Y/%m/%d') as daily").group("daily", :group_id).order("daily asc")
 
 	current = Time.current
 	logger.debug("current:#{current}")
@@ -44,16 +41,10 @@ class PersonalController < ApplicationController
 	@profit_dailys = @profit_dailys.select("DATE_FORMAT((finished_at - INTERVAL 12 HOUR + INTERVAL 9 HOUR), '%Y/%m') as month").having("month = ?", month)
 	logger.debug("@profit_dailys.length:#{@profit_dailys.length}")
 
-#	no_rates = Section.joins(:section_players).select("sections.*, section_players.*")
-#	no_rates = no_rates.joins(:group).select("groups.*")
-#	no_rates = no_rates.where(status: Section::Status::FINISHED, rate: nil)
-#	no_rates = no_rates.where("player_id = ?", player_ids)
-#	@no_rate_sections = no_rates
 
 	no_rates = sections
 	no_rates = no_rates.select("SUM(score) as sum_score")
 	no_rates = no_rates.select("COUNT(scorekind = #{Game::Scorekind::GAME} OR NULL) as count_game")
-	no_rates = no_rates.joins(:group).select("groups.*")
 	@no_rate_sections = no_rates.where(rate: nil).group("games.section_id")
 
 
